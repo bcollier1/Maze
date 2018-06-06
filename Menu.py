dee6f277215fffa69132e627e8d99be67f37244d
Brady Collier <brady_c_223@hotmail.com>
2018-06-06 08:08:14 -0400

    Fixed the to test files

--- Menu.py
+++ Menu.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame, sys
 from pygame.locals import *
 import tkinter
@@ -33,39 +32,4 @@ window.mainloop()
 
 
 
-=======
-import pygame, sys
-from pygame.locals import *
-import tkinter
-from tkinter import *
 
-def close():
-    exit()
-
-pygame.init()
-
-window = Tk()
-
-b1 = Button(window, text = "Start", command = close)
-b1.pack()
-
-#window.mainloop()
-
-
-
-menubar = Menu(window)
-
-filemenu = Menu(menubar, tearoff = 0)
-filemenu.add_command(label = "Close", command = close)
-
-menubar.add_cascade(label = "File", menu =filemenu)
-
-window.config(menu = menubar)
-window.mainloop()
-
-
-
-
-
-
->>>>>>> 30e6a374c87bfea24e7dcfcf251fb4195eb9f1a6
--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame, time, sys
 from shapes import Triangle, Rectangle, Oval
 from pygame.locals import *
@@ -32,39 +31,4 @@ while True:
     #print(mouse_x, mouse_y)
 
     pygame.display.update()
-=======
-import pygame, time, sys
-from shapes import Triangle, Rectangle, Oval
-from pygame.locals import *
-
-pygame.init()
-
-FPS=30
-fpsClock=pygame.time.Clock()
-
-width = 1080
-height = 720
-
-#pygame.Surface((width, height))
-
-DISPLAYSURF = pygame.display.set_mode((width,height))
-pygame.display.set_caption('Maze Game')
-background = pygame.image.load('maze.png')
-
-while True:
-
-    DISPLAYSURF.blit(background, (0, 0))
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
-    mouse_x,mouse_y = pygame.mouse.get_pos()
-    #pygame.Surface.get_at(mouse_x, mouse_y)
-    print(tuple(DISPLAYSURF.get_at((mouse_x, mouse_y))))
-    #print(mouse_x, mouse_y)
-
-    pygame.display.update()
->>>>>>> 30e6a374c87bfea24e7dcfcf251fb4195eb9f1a6
     fpsClock.tick(FPS)
